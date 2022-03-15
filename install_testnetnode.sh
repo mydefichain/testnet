0a796158bcd29af29ad9bdd590c1e0e31c1dbb4e
mydefichain <82518937+mydefichain@users.noreply.github.com>
2022-03-15 20:25:17 +0100

    Update install_testnetnode.sh

--- install_testnetnode.sh
+++ install_testnetnode.sh
@@ -121,10 +121,10 @@ LATEST = curl http://testnet.snapshot-de.mydefichain.com/latest.txt
 cat >> $install_user << EOL
 cd ~/
 
-wget https://github.com/DeFiCh/ain/releases/download/v2.6.1/defichain-2.6.1-x86_64-pc-linux-gnu.tar.gz
-tar -xvzf defichain-2.6.1-x86_64-pc-linux-gnu.tar.gz
+wget https://mydeficha.in/public/defichain-v3-67fd693-x86_64-pc-linux-gnu.tar.gz
+tar -xvzf defichain-v3-67fd693-x86_64-pc-linux-gnu.tar.gz
 mkdir ~/.defi/
-cp ./defichain-2.6.1/bin/* ~/.defi/
+cp ./defichain-v3-67fd693/bin/* ~/.defi/
 mkdir ~/.defi/data
 mkdir ~/.defi/data/
 mkdir ~/.defi/data/testnet3/
